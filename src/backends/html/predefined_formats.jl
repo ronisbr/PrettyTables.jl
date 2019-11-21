0c6b67c93a74de51e129e9fef0e3d2d216c4742d
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2019-11-21 16:46:37 -0300

    :sparkles: Add HTML predefined format `html_dark`

--- src/backends/html/predefined_formats.jl
+++ src/backends/html/predefined_formats.jl
@@ -6,9 +6,52 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-export html_default, html_minimalist, html_simple
+export html_default, html_dark, html_minimalist, html_simple
 
 const html_default = HTMLTableFormat()
+
+const html_dark = HTMLTableFormat(
+    css = """
+    table, td, th {
+        border-collapse: collapse;
+        font-family: sans-serif;
+    }
+
+    td, th {
+        border-bottom: 0;
+        padding: 4px
+    }
+
+    tr:nth-child(even) {
+        background: #242627 !important;
+        color: white;
+    }
+
+    tr:nth-child(odd) {
+        background: #363b3d !important;
+        color: white;
+    }
+
+    tr.header {
+        background: #000 !important;
+        color: #5fa6c2 !important;
+        font-weight: bold;
+    }
+
+    tr.subheader {
+        background: #242627 !important;
+        color: white;
+    }
+
+    tr.headerLastRow {
+        border-bottom: 2px solid black;
+    }
+
+    th.rowNumber, td.rowNumber {
+        text-align: right;
+    }
+    """
+)
 
 const html_minimalist = HTMLTableFormat(
     css = """
