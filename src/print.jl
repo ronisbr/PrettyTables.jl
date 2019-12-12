f84d44aaac68d5c283315038b812d3ed4c3e74b1
pdeffebach <23196228+pdeffebach@users.noreply.github.com>
2019-12-12 18:39:29 -0500

    :sparkles: Improve Tables.jl compatibility
    Closes #24

--- src/print.jl
+++ src/print.jl
@@ -353,13 +353,10 @@ pretty_table(table; kwargs...) = pretty_table(stdout, table; kwargs...)
 
 function pretty_table(io::IO, table; kwargs...)
 
-    # Get the data.
-    #
-    # If `table` is not compatible with Tables.jl, then an error will be thrown.
-    data = Tables.columns(table)
-
     # Get the table schema to obtain the columns names.
     sch = Tables.schema(table)
+    # Get the data.
+    data = Tables.matrix(table)
 
     if sch == nothing
         num_cols, num_rows = size(data)
