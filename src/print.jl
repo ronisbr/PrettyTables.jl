9bc8fa0f1b1c396651aba18a6d38adea0f38e675
pdeffebach <p.deffebach@gmail.com>
2019-12-12 16:38:27 -0500

    Add .gitignore

--- src/print.jl
+++ src/print.jl
@@ -352,7 +352,7 @@ function pretty_table(io::IO, table; kwargs...)
     # Get the data.
     #
     # If `table` is not compatible with Tables.jl, then an error will be thrown.
-    Tables.columns(t)  
+    Tables.columns(table)  
     # Get the table schema to obtain the columns names.
     sch = Tables.schema(table)
     data = Tables.matrix(table)
