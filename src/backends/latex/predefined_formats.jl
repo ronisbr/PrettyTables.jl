75713088199e5e389b8aa512207b20cc00e2e1d6
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2019-12-12 19:40:32 -0300

    :wrench: Remove thick lines in formats of LaTeX back-end
    The way the thick lines were being drawn was breaking the format
    when using `longtable`. Thus, if the user desire, then they must
    create a new format that uses an external package like `booktabs`.

--- src/backends/latex/predefined_formats.jl
+++ src/backends/latex/predefined_formats.jl
@@ -6,18 +6,18 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-export latex_default, latex_thick
+export latex_default, latex_simple
 
 const latex_default = LatexTableFormat()
 
-const latex_thick = LatexTableFormat(
-    top_line       = "\\noalign{\\hrule height 2pt}",
-    header_line    = "\\noalign{\\hrule height 2pt}",
-    mid_line       = "\\noalign{\\hrule height 1pt}",
-    bottom_line    = "\\noalign{\\hrule height 2pt}",
-    left_vline     = "@{\\vrule width 2pt\\hspace{2pt}}",
-    mid_vline      = "@{\\hspace{2pt}\\vrule width 2pt\\hspace{2pt}}",
-    right_vline    = "@{\\hspace{2pt}\\vrule width 2pt}",
+const latex_simple = LatexTableFormat(
+    top_line       = "\\hline",
+    header_line    = "\\hline",
+    mid_line       = "\\hline",
+    bottom_line    = "\\hline",
+    left_vline     = "|",
+    mid_vline      = "|",
+    right_vline    = "|",
     header_envs    = ["textbf"],
     subheader_envs = ["texttt"]
    )
