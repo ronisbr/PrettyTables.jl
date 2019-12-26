7d3da6c39e465a539939e8f8d7f651454c0c6361
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-12-26 15:22:52 +0000

    build based on 38fad9d

--- dev/search_index.js
+++ dev/search_index.js
@@ -37,7 +37,7 @@ var documenterSearchIndex = {"docs": [
     "page": "Home",
     "title": "Manual outline",
     "category": "section",
-    "text": "Pages = [\n    \"man/usage.md\"\n    \"man/text_backend.md\"\n    \"man/html_backend.md\"\n    \"man/alignment.md\"\n    \"man/filters.md\"\n    \"man/formatter.md\"\n    \"man/text_examples.md\"\n    \"man/html_examples.md\"\n    \"lib/library.md\"\n]\nDepth = 2"
+    "text": "Pages = [\n    \"man/usage.md\"\n    \"man/text_backend.md\"\n    \"man/html_backend.md\"\n    \"man/latex_backend.md\"\n    \"man/alignment.md\"\n    \"man/filters.md\"\n    \"man/formatter.md\"\n    \"man/text_examples.md\"\n    \"man/html_examples.md\"\n    \"lib/library.md\"\n]\nDepth = 2"
 },
 
 {
@@ -150,6 +150,38 @@ var documenterSearchIndex = {"docs": [
     "title": "HTML table formats",
     "category": "section",
     "text": "The following table formats are available when using the html back-end:html_default (Default)<iframe src=\"html_format_default.html\" frameborder=\"0\" scrolling=\"no\" onload=\"javascript:resizeIframe(this)\">\n  <p>Your browser does not support iframes. Click <a href=\"html_format_default.html>here</a> to see the table.</p>\n</iframe>html_dark<iframe src=\"html_format_dark.html\" frameborder=\"0\" scrolling=\"no\" onload=\"javascript:resizeIframe(this)\">\n  <p>Your browser does not support iframes. Click <a href=\"html_format_dark.html>here</a> to see the table.</p>\n</iframe>html_minimalist<iframe src=\"html_format_minimalist.html\" frameborder=\"0\" scrolling=\"no\" onload=\"javascript:resizeIframe(this)\">\n  <p>Your browser does not support iframes. Click <a href=\"html_format_minimalist.html>here</a> to see the table.</p>\n</iframe>html_simple<iframe src=\"html_format_simple.html\" frameborder=\"0\" scrolling=\"no\" onload=\"javascript:resizeIframe(this)\">\n  <p>Your browser does not support iframes. Click <a href=\"html_format_simple.html>here</a> to see the table.</p>\n</iframe>"
+},
+
+{
+    "location": "man/latex_backend/#",
+    "page": "LaTeX",
+    "title": "LaTeX",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "man/latex_backend/#LaTeX-back-end-1",
+    "page": "LaTeX",
+    "title": "LaTeX back-end",
+    "category": "section",
+    "text": "CurrentModule = PrettyTables\nDocTestSetup = quote\n    using PrettyTables\nendThe following options are available when the LaTeX backend is used. Those can be passed as keywords when calling the function pretty_table:cell_alignment: A dictionary of type (i,j) => a that overrides that                   alignment of the cell (i,j) to a regardless of the                   columns alignment selected. a must be a symbol like                   specified in the section Alignment.\nformatter: See the section Formatter.\nhighlighters: An instance of LatexHighlighter or a tuple with a                 list of LaTeX highlighters (see the section                 LaTeX highlighters).\nhlines: A vector of Int indicating row numbers in which an additional           horizontal line should be drawn after the row. Notice that numbers           lower than 1 and equal or higher than the number of rows will be           neglected.\nlongtable_footer: The string that will be drawn in the footer of the tables                     before a page break. This only works if table_type is                     :longtable. If it is nothing, then no footer will be                     used. (Default = nothing)\nnoheader: If true, then the header will not be printed. Notice that all             keywords and parameters related to the header and sub-headers will             be ignored. (Default = false)\nnosubheader: If true, then the sub-header will not be printed, i.e. the                header will contain only one line. Notice that this option has                no effect if noheader = true. (Default = false)\nrow_number_vline: If true, then a vertical line will be draw after the row                     number column. This only works if show_row_number is                     true. (Default = false)\nshow_row_number: If true, then a new column will be printed showing the                    row number. (Default = false)\ntable_type: Select which LaTeX environment will be used to print the table.               Currently supported options are :tabular for tabular or               :longtable for longtable. (Default = :tabular)\ntf: An instance of the structure LatexTableFormat that defines the       general format of the LaTeX table."
+},
+
+{
+    "location": "man/latex_backend/#LaTeX-highlighters-1",
+    "page": "LaTeX",
+    "title": "LaTeX highlighters",
+    "category": "section",
+    "text": "A set of highlighters can be passed as a Tuple to the highlighter keyword. Each highlighter is an instance of the structure LatexHighlighter. It contains the following two fields:f: Function with the signature f(data,i,j) in which should return true      if the element (i,j) in data must be highlighted, or false      otherwise.\nfd: A function with the signature f(data,i,j,str)::String in which       data is the matrix, (i,j) is the element position in the table, and       str is the data converted to string. This function must return a       string that will be placed in the cell.The function f has the following signature:f(data, i, j)in which data is a reference to the data that is being printed, i and j are the element coordinates that are being tested. If this function returns true, then the highlight style will be applied to the (i,j) element. Otherwise, the default style will be used.Notice that if multiple highlighters are valid for the element (i,j), then the applied style will be equal to the first match considering the order in the Tuple highlighters.If the function f returns true, then the function fd(data,i,j,str) will be called and must return the LaTeX string that will be placed in the cell.If only a single highlighter is wanted, then it can be passed directly to the keyword highlighter without being inside a Tuple.There are two helpers that can be used to create LaTeX highlighters:LatexHighlighter(f::Function, envs::Union{String,Vector{String}})\n\nLatexHighlighter(f::Function, fd::Function)The first will apply recursively all the LaTeX environments in envs to the highlighted text whereas the second let the user select the desired decoration by specifying the function fd.Thus, for example:LatexHighlighter((data,i,j)->true, [\"textbf\", \"small\"])will wrap all the cells in the table in the following environment:\\textbf{\\small{<Cell text>}}note: Note\nIf the highlighters are used together with Formatter, then the change in the format will not affect that parameter data passed to the highlighter function f. It will always receive the original, unformatted value.julia> t = 0:1:20;\n\njulia> data = hcat(t, ones(length(t))*1, 1*t, 0.5.*t.^2);\n\njulia> header = [\"Time\" \"Acceleration\" \"Velocity\" \"Distance\";\n                  \"[s]\"  \"[m/s\\$^2\\$]\"    \"[m/s]\"      \"[m]\"];\n\njulia> hl_v = LatexHighlighter( (data,i,j)->(j == 3) && data[i,3] > 9, [\"color{blue}\",\"textbf\"]);\n\njulia> hl_p = LatexHighlighter( (data,i,j)->(j == 4) && data[i,4] > 10, [\"color{red}\", \"textbf\"])\n\njulia> hl_e = LatexHighlighter( (data,i,j)->(i == 10), [\"cellcolor{black}\", \"color{white}\", \"textbf\"])\n\njulia> pretty_table(data, header, backend = :latex, highlighters = (hl_e, hl_p, hl_v))(Image: )note: Note\nThe following LaTeX packages are required to render this example: colortbl and xcolor."
+},
+
+{
+    "location": "man/latex_backend/#LaTeX-table-formats-1",
+    "page": "LaTeX",
+    "title": "LaTeX table formats",
+    "category": "section",
+    "text": "The following table formats are available when using the LaTeX back-end:latex_default (Default)(Image: )latex_simple(Image: )"
 },
 
 {
