996877ce3626a7600011b773657a2c4903f042a0
Dhruva Sambrani <44899822+DhruvaSambrani@users.noreply.github.com>
2020-01-30 00:03:23 +0530

    Update predefined_formats.jl

--- src/backends/latex/predefined_formats.jl
+++ src/backends/latex/predefined_formats.jl
@@ -6,7 +6,7 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
-export latex_default, latex_simple
+export latex_default, latex_simple, latex_matrix
 
 const latex_default = LatexTableFormat()
 
@@ -20,4 +20,14 @@ const latex_simple = LatexTableFormat(
     right_vline    = "|",
     header_envs    = ["textbf"],
     subheader_envs = ["texttt"]
+   )
+
+const latex_matrix = LatexTableFormat(
+    top_line       = " ",
+    header_line    = " ",
+    mid_line       = " ",
+    bottom_line    = " ",
+    left_vline     = "|",
+    mid_vline      = " ",
+    right_vline    = "|"
    )
