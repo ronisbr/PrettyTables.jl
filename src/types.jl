bca83c1e0ec64dd2c1ae608f26a9f35d6b189bd3
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-08-08 12:27:29 -0300

    :race_horse: Do not specialize the printing structures
    All the printing structures (`PrintInfo`, `ColumnTable`, and `RowTable`)
    were extremely specialized to avoid type instability. However, it turn
    out that the gain was not expressive since the bottleneck is actually
    the IO. Hence, by removing those specialization, we could save a lot the
    compilation time when printing different type of tables.
    
    Before:
    
        - First print of a 10x10 matrix: 3.13s
        - First print of a Dictionary with 3 values: 5.60s
        - First print of a 10x4 DataFrame: 1.26s
    
    After:
    
        - First print of a 10x10 matrix: 2.34s
        - First print of a Dictionary with 3 values: 3.93s
        - First print of a 10x4 DataFrame: 0.61s
    
    Futhremore, we can now use an inline formatter without a huge
    performance impact:
    
    Before:
    
    ```julia
    julia> @time pretty_table(df, formatters = (v,i,j) -> v)
      0.793990 seconds (2.95 M allocations: 155.715 MiB, 4.92% gc time)
    
    julia> @time pretty_table(df, formatters = (v,i,j) -> v == 1.0 ? "A" : "B")
      0.779655 seconds (2.95 M allocations: 155.709 MiB, 6.44% gc time)
    ```
    
    After:
    
    ```julia
    julia> @time pretty_table(df, formatters = (v,i,j) -> v)
      0.141214 seconds (285.12 k allocations: 16.487 MiB)
    
    julia> @time pretty_table(df, formatters = (v,i,j) -> v)
      0.184039 seconds (285.09 k allocations: 16.488 MiB, 26.31% gc time)
    ```
    
    The only downside is a very minor performance hit when printing the same
    table twice.

--- src/types.jl
+++ src/types.jl
@@ -7,31 +7,31 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 """
-    struct ColumnTable{T}
+    struct ColumnTable
 
 This structure helps to access elements that comply with the column access
 specification of Tables.jl.
 
 """
-struct ColumnTable{Td,Tt,V<:AbstractVector{Symbol}}
-    data::Td             # ...................................... Original table
-    table::Tt            # ............... Table converted using `Tables.column`
-    column_names::V      # ........................................ Column names
-    size::Tuple{Int,Int} # ................................... Size of the table
+struct ColumnTable
+    data         # .............................................. Original table
+    table        # ....................... Table converted using `Tables.column`
+    column_names # ................................................ Column names
+    size         # ........................................... Size of the table
 end
 
 """
-    struct RowTable{T}
+    struct RowTable
 
 This structure helps to access elements that comply with the row access
 specification of Tables.jl.
 
 """
-struct RowTable{Td,Tt,V<:AbstractVector{Symbol}}
-    data::Td             # ...................................... Original table
-    table::Tt            # ................. Table converted using `Tables.rows`
-    column_names::V      # ........................................ Column names
-    size::Tuple{Int,Int} # ................................... Size of the table
+struct RowTable
+    data         # .............................................. Original table
+    table        # ......................... Table converted using `Tables.rows`
+    column_names # ................................................ Column names
+    size         # ........................................... Size of the table
 end
 
 """
@@ -41,9 +41,9 @@ This structure stores the information required so that the backends can print
 the tables.
 
 """
-@with_kw struct PrintInfo{Td,Th,Trn,Tc <: Tuple, Tf <: Tuple}
-    data::Td
-    header::Th
+@with_kw struct PrintInfo
+    data
+    header
     id_cols::Vector{Int}
     id_rows::Vector{Int}
     num_rows::Int
@@ -53,11 +53,11 @@ the tables.
     header_num_rows::Int
     header_num_cols::Int
     show_row_names::Bool
-    row_names::Trn
+    row_names
     row_name_alignment::Symbol
     row_name_column_title::String
     alignment::Vector{Symbol}
-    cell_alignment::Tc
-    formatters::Tf
+    cell_alignment
+    formatters
     compact_printing::Bool
 end
