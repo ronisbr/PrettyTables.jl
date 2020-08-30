77dd9cff191e89bae15f5acf054672ac65081d60
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-08-29 22:22:01 -0300

    :rotating_light: Add test of the table format `dataframe`

--- test/text_backend.jl
+++ test/text_backend.jl
@@ -388,6 +388,21 @@ end
     result = pretty_table(String, data, tf = PrettyTables.compact)
     @test result == expected
 
+    # dataframe
+    # ==========================================================================
+    expected = """
+│ Col. 1 │ Col. 2 │ Col. 3 │ Col. 4 │
+├────────┼────────┼────────┼────────┤
+│      1 │  false │    1.0 │      1 │
+│      2 │   true │    2.0 │      2 │
+│      3 │  false │    3.0 │      3 │
+│      4 │   true │    4.0 │      4 │
+│      5 │  false │    5.0 │      5 │
+│      6 │   true │    6.0 │      6 │
+"""
+    result = pretty_table(String, data, tf = dataframe)
+    @test result == expected
+
     # markdown
     # ==========================================================================
     expected = """
