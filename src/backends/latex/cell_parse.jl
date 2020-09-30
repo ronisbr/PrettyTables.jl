79b09a9850a905af9d97c778d1fd39a9fc101dd4
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-09-29 22:36:53 -0300

    :wrench: Simplify cell parsing of HTML back-end

--- src/backends/latex/cell_parse.jl
+++ src/backends/latex/cell_parse.jl
@@ -15,13 +15,16 @@ will be printed to the IO.
 
 """
 @inline function _parse_cell_latex(cell;
+                                   cell_first_line_only::Bool = false,
                                    compact_printing::Bool = true,
                                    renderer::Union{Val{:print}, Val{:show}} = Val(:print),
                                    kwargs...)
 
     # Convert to string using the desired renderer.
     if renderer === Val(:show)
-        if showable(MIME("text/latex"), cell)
+        if cell isa AbstractString
+            cell_str = cell
+        elseif showable(MIME("text/latex"), cell)
             cell_str = sprint(show, MIME("text/latex"), cell;
                               context = :compact => compact_printing)
         else
@@ -32,25 +35,15 @@ will be printed to the IO.
         cell_str = sprint(print, cell; context = :compact => compact_printing)
     end
 
-    return cell_str
-end
-
-@inline _parse_cell_latex(cell::Markdown.MD; kwargs...) =
-    replace(sprint(show, MIME("text/latex"), cell),"\n"=>"")
-
-@inline function _parse_cell_latex(cell::AbstractString;
-                                   cell_first_line_only::Bool = false,
-                                   compact_printing::Bool = true,
-                                   kwargs...)
-
     if cell_first_line_only
-        cell_str = split(cell, '\n')[1]
-    else
-        cell_str = cell
+        cell_str = split(cell_str, '\n')[1]
     end
 
     return _str_latex_escaped(cell_str)
 end
+
+@inline _parse_cell_latex(cell::Markdown.MD; kwargs...) =
+    replace(sprint(show, MIME("text/latex"), cell),"\n"=>"")
 
 @inline _parse_cell_latex(cell::Missing; kwargs...) = "missing"
 @inline _parse_cell_latex(cell::Nothing; kwargs...) = "nothing"
