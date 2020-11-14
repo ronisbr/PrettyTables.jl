eeab3149f92425ebb4ff100b2bd3c183aadb8bc5
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2020-11-14 12:10:24 -0300

    :rotating_light: Fix tests in julia 1.0

--- test/general/issues.jl
+++ test/general/issues.jl
@@ -53,19 +53,19 @@
     @test result3 == expected
 end
 
+struct SimpleTable{T}
+    data::Matrix{T}
+end
+
+Tables.istable(::SimpleTable) = true
+Tables.columnaccess(::SimpleTable) = true
+Tables.columnnames(x::SimpleTable) = [Symbol(i) for i = 1:size(x.data, 2)]
+Tables.columns(x::SimpleTable) = x
+Tables.getcolumn(x::SimpleTable, i::Symbol) = tuple(x.data[parse(Int,string(i)),:]...)
+
+table = SimpleTable([10.0^(i+j) for i = 1:10, j = 1:5])
+
 @testset "Issue #90 - Tables.jl returning tuples as columns" begin
-    struct SimpleTable{T}
-        data::Matrix{T}
-    end
-
-    Tables.istable(::SimpleTable) = true
-    Tables.columnaccess(::SimpleTable) = true
-    Tables.columnnames(x::SimpleTable) = [Symbol(i) for i = 1:size(x.data, 2)]
-    Tables.columns(x::SimpleTable) = x
-    Tables.getcolumn(x::SimpleTable, i::Symbol) = tuple(x.data[parse(Int,string(i)),:]...)
-
-    table = SimpleTable([10.0^(i+j) for i = 1:10, j = 1:5])
-
     expected = """
 ┌──────────┬──────────┬──────────┬──────────┬────────┐
 │        1 │        2 │        3 │        4 │      5 │
--- test/general/tables.jl
+++ test/general/tables.jl
@@ -6,28 +6,28 @@
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
+struct TestVec{T} <: AbstractArray{T,1}
+    data::Array{T,1}
+end
+Base.IndexStyle(::Type{A}) where {A<:TestVec} = Base.IndexCartesian()
+Base.size(A::TestVec) = size(getfield(A, :data))
+Base.getindex(A::TestVec, index::Int) = getindex(getfield(A, :data), index)
+Base.collect(::Type{T}, itr::TestVec) where {T} = TestVec(collect(T, getfield(itr, :data)))
+
+struct MinimalTable
+    data::Matrix
+    colnames::TestVec
+end
+
+Tables.istable(x::MinimalTable) = true
+Tables.columnaccess(::MinimalTable) = true
+Tables.columnnames(x::MinimalTable) = getfield(x, :colnames)
+Tables.columns(x::MinimalTable) = x
+Base.getindex(x::MinimalTable, i1, i2) = getindex(getfield(x, :data), i1, i2)
+Base.getproperty(x::MinimalTable, s::Symbol) = getindex(x, :, findfirst(==(s), Tables.columnnames(x)))
+Base.convert(::Type{<:TestVec}, x::Array) = TestVec(x)
+
 @testset "Tables.jl with custom column name vector" begin
-    struct TestVec{T} <: AbstractArray{T,1}
-        data::Array{T,1}
-    end
-    Base.IndexStyle(::Type{A}) where {A<:TestVec} = Base.IndexCartesian()
-    Base.size(A::TestVec) = size(getfield(A, :data))
-    Base.getindex(A::TestVec, index::Int) = getindex(getfield(A, :data), index)
-    Base.collect(::Type{T}, itr::TestVec) where {T} = TestVec(collect(T, getfield(itr, :data)))
-
-    struct MinimalTable
-        data::Matrix
-        colnames::TestVec
-    end
-
-    Tables.istable(x::MinimalTable) = true
-    Tables.columnaccess(::MinimalTable) = true
-    Tables.columnnames(x::MinimalTable) = getfield(x, :colnames)
-    Tables.columns(x::MinimalTable) = x
-    Base.getindex(x::MinimalTable, i1, i2) = getindex(getfield(x, :data), i1, i2)
-    Base.getproperty(x::MinimalTable, s::Symbol) = getindex(x, :, findfirst(==(s), Tables.columnnames(x)))
-    Base.convert(::Type{<:TestVec}, x::Array) = TestVec(x)
-
     data     = [10.0^(i+j) for i = 1:10, j = 1:5]
     mintable = MinimalTable(data, [:C1, :C2, :C3, :C4, :C5])
 
