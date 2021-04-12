880e84ce77cca4f91573d008168c439a2509fe5a
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-04-12 14:04:33 -0300

    :bug: Add missing test file

---
+++ test/text_backend/errors.jl
@@ -0,0 +1,13 @@
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
+# Description
+# ==============================================================================
+#
+#    Tests of errors in text backend.
+#
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+@testset "Errors in keywords" begin
+    @test_throws Exception pretty_table([1 2], header_crayon = [crayon"white"])
+    @test_throws Exception pretty_table([1 2], subheader_crayon = [crayon"white"])
+end
