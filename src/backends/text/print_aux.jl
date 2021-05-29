506eb51fdebe50ceac3b2942fd0185acff07c4f6
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-29 18:40:06 -0300

    :art: Apply BlueStyle to s/b/l/print_aux.jl

--- src/backends/text/print_aux.jl
+++ src/backends/text/print_aux.jl
@@ -8,12 +8,13 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
 
 # Flush the content of `buf` to `io`.
-function _flush_buffer!(io::IO,
-                        buf::IO,
-                        overwrite::Bool,
-                        newline_at_end::Bool,
-                        num_displayed_rows::Int)
-
+function _flush_buffer!(
+    io::IO,
+    buf::IO,
+    overwrite::Bool,
+    newline_at_end::Bool,
+    num_displayed_rows::Int
+)
     # If `overwrite` is `true`, then delete the exact number of lines of the
     # table. This can be used to replace the table in the display continuously.
     str_overwrite = overwrite ? "\e[1F\e[2K"^(num_displayed_rows - 1) : ""
@@ -28,14 +29,15 @@ function _flush_buffer!(io::IO,
     return nothing
 end
 
-function _print_omitted_cell_summary(buf::IO,
-                                     has_color::Bool,
-                                     num_omitted_cols::Int,
-                                     num_omitted_rows::Int,
-                                     omitted_cell_summary_crayon::Crayon,
-                                     show_omitted_cell_summary::Bool,
-                                     table_width::Int)
-
+function _print_omitted_cell_summary(
+    buf::IO,
+    has_color::Bool,
+    num_omitted_cols::Int,
+    num_omitted_rows::Int,
+    omitted_cell_summary_crayon::Crayon,
+    show_omitted_cell_summary::Bool,
+    table_width::Int
+)
     if show_omitted_cell_summary && ((num_omitted_cols + num_omitted_rows) > 0)
         cs_str_col = ""
         cs_str_and = ""
@@ -55,41 +57,45 @@ function _print_omitted_cell_summary(buf::IO,
 
         cs_str = cs_str_col * cs_str_and * cs_str_row * " omitted"
 
-        textwidth(cs_str) < table_width &&
-            (cs_str = _str_aligned(cs_str, :r, table_width))
+        if textwidth(cs_str) < table_width
+            cs_str = _str_aligned(cs_str, :r, table_width)
+        end
 
         has_color && print(buf, omitted_cell_summary_crayon)
         print(buf, cs_str)
         has_color && print(buf, _reset_crayon)
         println(buf)
     end
+
+    return nothing
 end
 
 # Print the table header without the horizontal lines.
-function _print_table_header!(buf::IO,
-                              display::Display,
-                              (@nospecialize header::Any),
-                              header_str::Matrix{String},
-                              id_cols::Vector{Int},
-                              id_rows::Vector{Int},
-                              num_printed_cols::Int,
-                              Δc::Int,
-                              cols_width::Vector{Int},
-                              vlines::Vector{Int},
-                              # Configurations.
-                              alignment::Vector{Symbol},
-                              header_alignment::Vector{Symbol},
-                              (@nospecialize header_cell_alignment::Ref{Any}),
-                              show_row_names::Bool,
-                              show_row_number::Bool,
-                              tf::TextFormat,
-                              # Crayons.
-                              border_crayon::Crayon,
-                              header_crayon::Vector{Crayon},
-                              subheader_crayon::Vector{Crayon},
-                              rownum_header_crayon::Crayon,
-                              row_name_header_crayon::Crayon)
-
+function _print_table_header!(
+    buf::IO,
+    display::Display,
+    (@nospecialize header::Any),
+    header_str::Matrix{String},
+    id_cols::Vector{Int},
+    id_rows::Vector{Int},
+    num_printed_cols::Int,
+    Δc::Int,
+    cols_width::Vector{Int},
+    vlines::Vector{Int},
+    # Configurations.
+    alignment::Vector{Symbol},
+    header_alignment::Vector{Symbol},
+    (@nospecialize header_cell_alignment::Ref{Any}),
+    show_row_names::Bool,
+    show_row_number::Bool,
+    tf::TextFormat,
+    # Crayons.
+    border_crayon::Crayon,
+    header_crayon::Vector{Crayon},
+    subheader_crayon::Vector{Crayon},
+    rownum_header_crayon::Crayon,
+    row_name_header_crayon::Crayon
+)
     header_num_rows, ~ = size(header_str)
 
     @inbounds @views for i = 1:header_num_rows
@@ -122,14 +128,12 @@ function _print_table_header!(buf::IO,
 
                 # If alignment is `:s`, then we must use the column
                 # alignment.
-                alignment_ij ∈ (:s,:S) && (alignment_ij = alignment[jc+Δc])
+                alignment_ij ∈ (:s, :S) && (alignment_ij = alignment[jc + Δc])
             end
 
             # Prepare the text to be printed.
-            header_ij_str = _str_aligned(header_str[i,j],
-                                         alignment_ij,
-                                         cols_width[j])
-            header_ij_str  = " " * header_ij_str * " "
+            header_ij_str = _str_aligned(header_str[i, j], alignment_ij, cols_width[j])
+            header_ij_str = " " * header_ij_str * " "
 
             flp = j == num_printed_cols
 
@@ -146,47 +150,63 @@ function _print_table_header!(buf::IO,
     end
 
     _nl!(display, buf)
+
+    return nothing
 end
 
 # Print the entire table data.
-function _print_table_data(buf::IO,
-                           display::Display,
-                           (@nospecialize data::Any),
-                           data_str::Matrix{Vector{String}},
-                           id_cols::Vector{Int},
-                           id_rows::Vector{Int},
-                           Δc::Int,
-                           cols_width::Vector{Int},
-                           vlines::Vector{Int},
-                           row_printing_recipe::Vector{NTuple{4, Int}},
-                           col_printing_recipe::Vector{Int},
-                           # Configurations.
-                           alignment::Vector{Symbol},
-                           body_hlines_format::NTuple{4, Char},
-                           cell_alignment_override::Dict{Tuple{Int, Int}, Symbol},
-                           continuation_row_alignment::Symbol,
-                           ellipsis_line_skip::Integer,
-                           (@nospecialize highlighters::Ref{Any}),
-                           noheader::Bool,
-                           show_row_names::Bool,
-                           show_row_number::Bool,
-                           tf::TextFormat,
-                           # Crayons.
-                           border_crayon::Crayon,
-                           row_name_crayon::Crayon,
-                           text_crayon::Crayon)
-
+function _print_table_data(
+    buf::IO,
+    display::Display,
+    (@nospecialize data::Any),
+    data_str::Matrix{Vector{String}},
+    id_cols::Vector{Int},
+    id_rows::Vector{Int},
+    Δc::Int,
+    cols_width::Vector{Int},
+    vlines::Vector{Int},
+    row_printing_recipe::Vector{NTuple{4, Int}},
+    col_printing_recipe::Vector{Int},
+    # Configurations.
+    alignment::Vector{Symbol},
+    body_hlines_format::NTuple{4, Char},
+    cell_alignment_override::Dict{Tuple{Int, Int}, Symbol},
+    continuation_row_alignment::Symbol,
+    ellipsis_line_skip::Integer,
+    (@nospecialize highlighters::Ref{Any}),
+    noheader::Bool,
+    show_row_names::Bool,
+    show_row_number::Bool,
+    tf::TextFormat,
+    # Crayons.
+    border_crayon::Crayon,
+    row_name_crayon::Crayon,
+    text_crayon::Crayon
+)
     line_count = 0
 
     for r in row_printing_recipe
         if r[1] < 0
             if r[1] == _ROW_LINE[1]
-                _draw_line!(display, buf, body_hlines_format..., border_crayon,
-                            cols_width, vlines)
+                _draw_line!(
+                    display,
+                    buf,
+                    body_hlines_format...,
+                    border_crayon,
+                    cols_width,
+                    vlines
+                )
             elseif r[1] == _CONTINUATION_LINE[1]
-                _draw_continuation_row(display, buf, tf, text_crayon,
-                                       border_crayon, cols_width, vlines,
-                                       continuation_row_alignment)
+                _draw_continuation_row(
+                    display,
+                    buf,
+                    tf,
+                    text_crayon,
+                    border_crayon,
+                    cols_width,
+                    vlines,
+                    continuation_row_alignment
+                )
             else
                 error("Internal error: wrong symbol in row printing recipe.")
             end
@@ -194,11 +214,15 @@ function _print_table_data(buf::IO,
         else
             i, ir, num_lines, l₀ = r
 
-            for l = l₀:(l₀+num_lines-1)
+            for l = l₀:(l₀ + num_lines - 1)
 
                 # Check if we should print the ellipsis here.
-                display.cont_char =
-                    line_count % (ellipsis_line_skip + 1) == 0 ? '⋯' : ' '
+                display.cont_char = if line_count % (ellipsis_line_skip + 1) == 0
+                    '⋯'
+                else
+                    ' '
+                end
+
                 line_count += 1
 
                 num_col_recipes = length(col_printing_recipe)
@@ -238,15 +262,16 @@ function _print_table_data(buf::IO,
                             jc = 0
                             data_cell = false
                         else
-                            jc = id_cols[j-Δc]
+                            jc = id_cols[j - Δc]
+
                             crayon_ij    = text_crayon
-                            alignment_ij = alignment[jc+Δc]
+                            alignment_ij = alignment[jc + Δc]
                             data_cell    = true
                         end
 
                         # String to be processed.
-                        if length(data_str[i,j]) >= l
-                            data_ij_str = data_str[i,j][l]
+                        if length(data_str[i, j]) >= l
+                            data_ij_str = data_str[i, j][l]
                         else
                             data_ij_str = ""
                         end
@@ -259,16 +284,17 @@ function _print_table_data(buf::IO,
 
                         # Process the string with the correct alignment and also
                         # apply the highlighters.
-                        data_ij_str, crayon_ij =
-                            _process_cell_text(data,
-                                               ir,
-                                               jc,
-                                               data_cell,
-                                               data_ij_str,
-                                               cols_width[j],
-                                               crayon_ij,
-                                               alignment_ij,
-                                               highlighters)
+                        data_ij_str, crayon_ij = _process_cell_text(
+                            data,
+                            ir,
+                            jc,
+                            data_cell,
+                            data_ij_str,
+                            cols_width[j],
+                            crayon_ij,
+                            alignment_ij,
+                            highlighters
+                        )
 
                         # Compute the printable size of the string.
                         data_ij_len = _printable_textwidth(data_ij_str)
@@ -276,8 +302,11 @@ function _print_table_data(buf::IO,
                         _p!(display, crayon_ij, data_ij_str, false, data_ij_len)
                     end
                 end
+
                 _nl!(display, buf)
             end
         end
     end
+
+    return nothing
 end
