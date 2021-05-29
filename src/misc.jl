ee91a9e72201d6caa5ec8e31eb2205afc6f927de
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-29 14:20:46 -0300

    :art: Apply BlueStyle to s/misc.jl

--- src/misc.jl
+++ src/misc.jl
@@ -1,5 +1,7 @@
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#
 # Description
+# ==============================================================================
 #
 #   Miscellaneous functions used by all back-ends.
 #
@@ -11,13 +13,16 @@ export compact_type_str
     compact_type_str(T)
 
 Return a string with a compact representation of type `T`.
-
 """
 compact_type_str(T) = string(T)
 
 function compact_type_str(T::Union)
     if VERSION < v"1.3.0"
-        str = T >: Missing ? string(Core.Compiler.typesubtract(T, Missing)) * "?" : string(T)
+        str = if T >: Missing
+            string(Core.Compiler.typesubtract(T, Missing)) * "?"
+        else
+            string(T)
+        end
     else
         str = T >: Missing ? string(nonmissingtype(T)) * "?" : string(T)
     end
@@ -37,11 +42,10 @@ Print the variable `v` to the buffer `buf` at the indentation level
 `indentation`. Each level has `nspaces` spaces.
 
 If `v` is not present, then only the indentation spaces will be printed.
-
 """
 function _aprint(buf::IO, v::String, indentation::Int = 0, nspaces::Int = 2)
     tokens  = split(v, '\n')
-    padding = " "^(indentation*nspaces)
+    padding = " "^(indentation * nspaces)
     ntokens = length(tokens)
 
     @inbounds for i = 1:ntokens
@@ -53,22 +57,25 @@ function _aprint(buf::IO, v::String, indentation::Int = 0, nspaces::Int = 2)
         end
         i != ntokens && println(buf)
     end
+
+    return nothing
 end
 
 function _aprint(buf::IO, indentation::Int = 0, nspaces::Int = 2)
     padding = " "^(indentation*nspaces)
     print(buf, padding)
+
+    return nothing
 end
 
 """
     _aprintln(buf::IO, [v,] indentation = 0, nspaces = 2)
 
 Same as `_aprint`, but a new line will be added at the end.
-
 """
 function _aprintln(buf::IO, v::String, indentation::Int = 0, nspaces::Int = 2)
     tokens  = split(v, '\n')
-    padding = " "^(indentation*nspaces)
+    padding = " "^(indentation * nspaces)
     ntokens = length(tokens)
 
     @inbounds for i = 1:ntokens
@@ -81,11 +88,15 @@ function _aprintln(buf::IO, v::String, indentation::Int = 0, nspaces::Int = 2)
             println(buf)
         end
     end
+
+    return nothing
 end
 
 function _aprintln(buf::IO, indentation::Int = 0, nspaces::Int = 2)
     padding = " "^(indentation*nspaces)
     println(buf, padding)
+
+    return nothing
 end
 
 """
@@ -96,10 +107,13 @@ number of printed rows `num_printed_rows` and if the header is present
 (`noheader`).
 
 It returns a vector of `Int` stating where the horizontal lines must be drawn.
-
 """
-@inline function _process_hlines(hlines::Symbol, body_hlines::AbstractVector,
-                         num_printed_rows::Int, noheader::Bool)
+@inline function _process_hlines(
+    hlines::Symbol,
+    body_hlines::AbstractVector,
+    num_printed_rows::Int,
+    noheader::Bool
+)
     if hlines == :all
         vhlines = collect(0:1:num_printed_rows + !noheader)
     elseif hlines == :none
@@ -111,14 +125,20 @@ It returns a vector of `Int` stating where the horizontal lines must be drawn.
     return _process_hlines(vhlines, body_hlines, num_printed_rows, noheader)
 end
 
-function _process_hlines(hlines::AbstractVector, body_hlines::AbstractVector,
-                         num_printed_rows::Int, noheader::Bool)
-
+function _process_hlines(
+    hlines::AbstractVector,
+    body_hlines::AbstractVector,
+    num_printed_rows::Int,
+    noheader::Bool
+)
     # The symbol `:begin` is replaced by 0, the symbol `:header` by the line
     # after the header, and the symbol `:end` is replaced by the last row.
-    hlines = replace(hlines, :begin  => 0,
-                             :header => noheader ? -1 : 1,
-                             :end    => num_printed_rows + !noheader)
+    hlines = replace(
+        hlines,
+        :begin  => 0,
+        :header => noheader ? -1 : 1,
+        :end    => num_printed_rows + !noheader
+    )
 
     # All numbers less than 1 and higher or equal the number of printed rows
     # must be # removed from `body_hlines`.
@@ -144,7 +164,6 @@ Process the vertical lines `vlines` considerering the number of printed columns
 `num_printed_cols`.
 
 It returns a vector of `Int` stating where the vertical lines must be drawn.
-
 """
 @inline function _process_vlines(vlines::Symbol, num_printed_cols::Int)
     # Process `vlines`.
@@ -171,8 +190,11 @@ function _process_vlines(vlines::AbstractVector, num_printed_cols::Int)
 
     # The symbol `:begin` is replaced by 0 and the symbol `:end` is replaced by
     # the last column.
-    vlines = replace(vlines, :begin => 0,
-                             :end   => num_printed_cols)
+    vlines = replace(
+        vlines,
+        :begin => 0,
+        :end   => num_printed_cols
+    )
 
     return Vector{Int}(vlines)
 end
@@ -183,7 +205,8 @@ end
 Return the escaped string representation of `str`.
 
 """
-_str_escaped(str::AbstractString) =
+function _str_escaped(str::AbstractString)
     # NOTE: Here we cannot use `escape_string(str)` because it also adds the
     # character `"` to the list of characters to be escaped.
-    sprint(escape_string, str, "", sizehint = lastindex(str))
+    return sprint(escape_string, str, "", sizehint = lastindex(str))
+end
