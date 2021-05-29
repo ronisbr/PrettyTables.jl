57d3439d0573b6b7e6eff9ff5385c7a080486a2f
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-05-29 14:49:18 -0300

    :art: Apply BlueStyle to s/private.jl

--- src/private.jl
+++ src/private.jl
@@ -14,8 +14,10 @@
 # Those functions apply a preprocessing to the data that will be printed
 # depending on its type.
 
-function _preprocess_vec_or_mat(data::AbstractVecOrMat,
-                                header::Union{Nothing, AbstractVector, Tuple})
+function _preprocess_vec_or_mat(
+    data::AbstractVecOrMat,
+    header::Union{Nothing, AbstractVector, Tuple}
+)
     if header === nothing
         pheader = (["Col. " * string(i) for i = 1:size(data,2)],)
     elseif header isa AbstractVector
@@ -27,10 +29,14 @@ function _preprocess_vec_or_mat(data::AbstractVecOrMat,
     return data, pheader
 end
 
-function _preprocess_dict(dict::AbstractDict{K, V}; sortkeys::Bool = false) where {K, V}
-    pheader =
-        (["Keys",              "Values"],
-         [compact_type_str(K), compact_type_str(V)])
+function _preprocess_dict(
+    dict::AbstractDict{K, V};
+    sortkeys::Bool = false
+) where {K, V}
+    pheader = (
+        ["Keys", "Values"],
+        [compact_type_str(K), compact_type_str(V)]
+    )
 
     k = collect(keys(dict))
     v = collect(values(dict))
@@ -49,9 +55,10 @@ function _preprocess_dict(dict::AbstractDict{K, V}; sortkeys::Bool = false) wher
     return pdata, pheader
 end
 
-function _preprocess_Tables_column(data::Any,
-                                   header::Union{Nothing, AbstractVector, Tuple})
-
+function _preprocess_Tables_column(
+    data::Any,
+    header::Union{Nothing, AbstractVector, Tuple}
+)
     # Access the table using the columns.
     table = Tables.columns(data)
 
@@ -89,9 +96,10 @@ function _preprocess_Tables_column(data::Any,
     return pdata, pheader
 end
 
-function _preprocess_Tables_row(data::Any,
-                                header::Union{Nothing, AbstractVector, Tuple})
-
+function _preprocess_Tables_row(
+    data::Any,
+    header::Union{Nothing, AbstractVector, Tuple}
+)
     # Access the table using the rows.
     table = Tables.rows(data)
 
@@ -137,33 +145,38 @@ end
 ################################################################################
 
 # This function creates the structure that holds the global print information.
-function _print_info(data::Any;
-                     alignment::Union{Symbol,Vector{Symbol}} = :r,
-                     cell_alignment::Union{Nothing,
-                                           Dict{Tuple{Int,Int},Symbol},
-                                           Function,
-                                           Tuple} = nothing,
-                     cell_first_line_only::Bool = false,
-                     compact_printing::Bool = true,
-                     filters_row::Union{Nothing,Tuple} = nothing,
-                     filters_col::Union{Nothing,Tuple} = nothing,
-                     formatters::Union{Nothing,Function,Tuple} = nothing,
-                     header::Union{Nothing, AbstractVector, Tuple} = nothing,
-                     header_alignment::Union{Symbol,Vector{Symbol}} = :s,
-                     header_cell_alignment::Union{Nothing,
-                                                  Dict{Tuple{Int,Int},Symbol},
-                                                  Function,
-                                                  Tuple} = nothing,
-                     limit_printing::Bool = true,
-                     renderer::Symbol = :print,
-                     row_names::Union{Nothing,AbstractVector} = nothing,
-                     row_name_alignment::Symbol = :r,
-                     row_name_column_title::AbstractString = "",
-                     row_number_column_title::AbstractString = "Row",
-                     show_row_number::Bool = false,
-                     title::AbstractString = "",
-                     title_alignment::Symbol = :l)
-
+function _print_info(
+    data::Any;
+    alignment::Union{Symbol, Vector{Symbol}} = :r,
+    cell_alignment::Union{
+        Nothing,
+        Dict{Tuple{Int, Int}, Symbol},
+        Function,
+        Tuple
+    } = nothing,
+    cell_first_line_only::Bool = false,
+    compact_printing::Bool = true,
+    filters_row::Union{Nothing, Tuple} = nothing,
+    filters_col::Union{Nothing, Tuple} = nothing,
+    formatters::Union{Nothing, Function, Tuple} = nothing,
+    header::Union{Nothing, AbstractVector, Tuple} = nothing,
+    header_alignment::Union{Symbol, Vector{Symbol}} = :s,
+    header_cell_alignment::Union{
+        Nothing,
+        Dict{Tuple{Int, Int}, Symbol},
+        Function,
+        Tuple
+    } = nothing,
+    limit_printing::Bool = true,
+    renderer::Symbol = :print,
+    row_names::Union{Nothing, AbstractVector} = nothing,
+    row_name_alignment::Symbol = :r,
+    row_name_column_title::AbstractString = "",
+    row_number_column_title::AbstractString = "Row",
+    show_row_number::Bool = false,
+    title::AbstractString = "",
+    title_alignment::Symbol = :l
+)
     # Get information about the table we have to print based on the format of
     # `data`, which must be an `AbstractMatrix` or an `AbstractVector`.
     dims     = size(data)
@@ -187,20 +200,26 @@ function _print_info(data::Any;
     if alignment isa Symbol
         alignment = fill(alignment, num_cols)
     else
-        length(alignment) != num_cols && error("The length of `alignment` must be the same as the number of rows.")
+        if length(alignment) != num_cols
+            error("The length of `alignment` must be the same as the number of rows.")
+        end
     end
 
     if header_alignment isa Symbol
         header_alignment = fill(header_alignment, num_cols)
     else
-        length(header_alignment) != num_cols && error("The length of `header_alignment` must be the same as the number of rows.")
+        if length(header_alignment) != num_cols
+            error("The length of `header_alignment` must be the same as the number of rows.")
+        end
     end
 
     # If there is a vector of row names, then it must have the same size of the
     # number of rows.
     if row_names != nothing
-        length(row_names) != num_rows &&
-        error("The number of lines in `row_names` must match the number of lines in the matrix.")
+        if length(row_names) != num_rows
+            error("The number of lines in `row_names` must match the number of lines in the matrix.")
+        end
+
         show_row_names = true
     else
         show_row_names = false
@@ -217,7 +236,7 @@ function _print_info(data::Any;
             filtered_i = true
 
             for filter in filters_row
-                !filter(_getdata(data),i) && (filtered_i = false) && break
+                !filter(_getdata(data), i) && (filtered_i = false) && break
             end
 
             filtered_rows[i] = filtered_i
@@ -229,7 +248,7 @@ function _print_info(data::Any;
             filtered_i = true
 
             for filter in filters_col
-                !filter(_getdata(data),i) && (filtered_i = false) && break
+                !filter(_getdata(data), i) && (filtered_i = false) && break
             end
 
             filtered_cols[i] = filtered_i
@@ -251,9 +270,9 @@ function _print_info(data::Any;
         # alignment for the cell `(i,j)`. Thus, we need to create a wrapper
         # function.
         cell_alignment_dict = copy(cell_alignment)
-        cell_alignment = ((data,i,j) -> begin
-            if haskey(cell_alignment_dict, (i,j))
-                return cell_alignment_dict[(i,j)]
+        cell_alignment = ((data, i, j) -> begin
+            if haskey(cell_alignment_dict, (i, j))
+                return cell_alignment_dict[(i, j)]
             else
                 return nothing
             end
@@ -270,9 +289,9 @@ function _print_info(data::Any;
         # desired alignment for the cell `(i,j)`. Thus, we need to create a
         # wrapper function.
         header_cell_alignment_dict = copy(header_cell_alignment)
-        header_cell_alignment = ((data,i,j) -> begin
-            if haskey(header_cell_alignment_dict, (i,j))
-                return header_cell_alignment_dict[(i,j)]
+        header_cell_alignment = ((data, i, j) -> begin
+            if haskey(header_cell_alignment_dict, (i, j))
+                return header_cell_alignment_dict[(i, j)]
             else
                 return nothing
             end
@@ -289,14 +308,35 @@ function _print_info(data::Any;
     renderer_val = renderer == :show ? Val(:show) : Val(:print)
 
     # Create the structure that stores the print information.
-    pinfo = PrintInfo(data, _header, id_cols, id_rows, num_rows, num_cols,
-                      num_printed_cols, num_printed_rows, header_num_rows,
-                      header_num_cols, show_row_number, row_number_column_title,
-                      show_row_names, row_names, row_name_alignment,
-                      row_name_column_title, alignment, cell_alignment,
-                      formatters, compact_printing, title, title_alignment,
-                      header_alignment, header_cell_alignment,
-                      cell_first_line_only, renderer_val, limit_printing)
+    pinfo = PrintInfo(
+        data,
+        _header,
+        id_cols,
+        id_rows,
+        num_rows,
+        num_cols,
+        num_printed_cols,
+        num_printed_rows,
+        header_num_rows,
+        header_num_cols,
+        show_row_number,
+        row_number_column_title,
+        show_row_names,
+        row_names,
+        row_name_alignment,
+        row_name_column_title,
+        alignment,
+        cell_alignment,
+        formatters,
+        compact_printing,
+        title,
+        title_alignment,
+        header_alignment,
+        header_cell_alignment,
+        cell_first_line_only,
+        renderer_val,
+        limit_printing
+    )
 
     return pinfo
 end
@@ -307,15 +347,18 @@ end
 
 # This is a middleware function to apply the preprocess step to the data that
 # will be printed.
-function _pretty_table((@nospecialize io::IO), data::Any;
-                       header::Union{Nothing, AbstractVector, Tuple} = nothing,
-                       kwargs...)
+function _pretty_table(
+    (@nospecialize io::IO),
+    data::Any;
+    header::Union{Nothing, AbstractVector, Tuple} = nothing,
+    kwargs...
+)
 
     if Tables.istable(data)
         if Tables.columnaccess(data)
             pdata, pheader = _preprocess_Tables_column(data, header)
         elseif Tables.rowaccess(data)
-            pdata, pheader = _preprocess_Tables_row(data,header)
+            pdata, pheader = _preprocess_Tables_row(data, header)
         else
             error("The object does not have a valid Tables.jl implementation.")
         end
@@ -335,41 +378,49 @@ end
 # This is the low level function that prints the table. In this case, `data`
 # must be accessed by `[i,j]` and the size of the `header` must be equal to the
 # number of columns in `data`.
-function _pt((@nospecialize io::IO), data::Any;
-             alignment::Union{Symbol,Vector{Symbol}} = :r,
-             backend::Union{Symbol, T_BACKENDS} = Val(:auto),
-             cell_alignment::Union{Nothing,
-                                   Dict{Tuple{Int,Int},Symbol},
-                                   Function,
-                                   Tuple} = nothing,
-             cell_first_line_only::Bool = false,
-             compact_printing::Bool = true,
-             filters_row::Union{Nothing,Tuple} = nothing,
-             filters_col::Union{Nothing,Tuple} = nothing,
-             formatters::Union{Nothing,Function,Tuple} = nothing,
-             header::Union{Nothing, AbstractVector, Tuple} = nothing,
-             header_alignment::Union{Symbol,Vector{Symbol}} = :s,
-             header_cell_alignment::Union{Nothing,
-                                          Dict{Tuple{Int,Int},Symbol},
-                                          Function,
-                                          Tuple} = nothing,
-             limit_printing::Bool = true,
-             renderer::Symbol = :print,
-             row_names::Union{Nothing,AbstractVector} = nothing,
-             row_name_alignment::Symbol = :r,
-             row_name_column_title::AbstractString = "",
-             row_number_column_title::AbstractString = "Row",
-             show_row_number::Bool = false,
-             title::AbstractString = "",
-             title_alignment::Symbol = :l,
-             kwargs...)
-
+function _pt(
+    (@nospecialize io::IO),
+    data::Any;
+    alignment::Union{Symbol, Vector{Symbol}} = :r,
+    backend::Union{Symbol, T_BACKENDS} = Val(:auto),
+    cell_alignment::Union{
+        Nothing,
+        Dict{Tuple{Int, Int}, Symbol},
+        Function,
+        Tuple
+    } = nothing,
+    cell_first_line_only::Bool = false,
+    compact_printing::Bool = true,
+    filters_row::Union{Nothing, Tuple} = nothing,
+    filters_col::Union{Nothing, Tuple} = nothing,
+    formatters::Union{Nothing, Function, Tuple} = nothing,
+    header::Union{Nothing, AbstractVector, Tuple} = nothing,
+    header_alignment::Union{Symbol, Vector{Symbol}} = :s,
+    header_cell_alignment::Union{
+        Nothing,
+        Dict{Tuple{Int, Int}, Symbol},
+        Function,
+        Tuple
+    } = nothing,
+    limit_printing::Bool = true,
+    renderer::Symbol = :print,
+    row_names::Union{Nothing, AbstractVector} = nothing,
+    row_name_alignment::Symbol = :r,
+    row_name_column_title::AbstractString = "",
+    row_number_column_title::AbstractString = "Row",
+    show_row_number::Bool = false,
+    title::AbstractString = "",
+    title_alignment::Symbol = :l,
+    kwargs...
+)
     # NOTE: Inform that using `backend` as `Symbol` is deprecated. This will be
     # removed in the next PrettyTables.jl version.
     if backend isa Symbol
       _backend = Val(backend)
-      Base.depwarn("Using `backend` as a symbol is deprecated. Use `Val(:$backend)` instead.",
-                   nothing)
+      Base.depwarn(
+          "Using `backend` as a symbol is deprecated. Use `Val(:$backend)` instead.",
+          nothing
+      )
     else
       _backend = backend
     end
@@ -387,11 +438,13 @@ function _pt((@nospecialize io::IO), data::Any;
             elseif tf isa LatexTableFormat
                 _backend = Val(:latex)
             else
-                throw( TypeError(:_pt,
-                                 Union{TextFormat,
-                                       HTMLTableFormat,
-                                       LatexTableFormat},
-                                 typeof(tf)))
+                throw(
+                    TypeError(
+                        :_pt,
+                        Union{TextFormat, HTMLTableFormat, LatexTableFormat},
+                        typeof(tf)
+                    )
+                )
             end
         else
             _backend = Val(:text)
@@ -399,26 +452,28 @@ function _pt((@nospecialize io::IO), data::Any;
     end
 
     # Create the structure that stores the print information.
-    pinfo = _print_info(data;
-                        alignment               = alignment,
-                        cell_alignment          = cell_alignment,
-                        cell_first_line_only    = cell_first_line_only,
-                        compact_printing        = compact_printing,
-                        filters_row             = filters_row,
-                        filters_col             = filters_col,
-                        formatters              = formatters,
-                        header                  = header,
-                        header_alignment        = header_alignment,
-                        header_cell_alignment   = header_cell_alignment,
-                        limit_printing          = limit_printing,
-                        renderer                = renderer,
-                        row_names               = row_names,
-                        row_name_alignment      = row_name_alignment,
-                        row_name_column_title   = row_name_column_title,
-                        row_number_column_title = row_number_column_title,
-                        show_row_number         = show_row_number,
-                        title                   = title,
-                        title_alignment         = title_alignment)
+    pinfo = _print_info(
+        data;
+        alignment               = alignment,
+        cell_alignment          = cell_alignment,
+        cell_first_line_only    = cell_first_line_only,
+        compact_printing        = compact_printing,
+        filters_row             = filters_row,
+        filters_col             = filters_col,
+        formatters              = formatters,
+        header                  = header,
+        header_alignment        = header_alignment,
+        header_cell_alignment   = header_cell_alignment,
+        limit_printing          = limit_printing,
+        renderer                = renderer,
+        row_names               = row_names,
+        row_name_alignment      = row_name_alignment,
+        row_name_column_title   = row_name_column_title,
+        row_number_column_title = row_number_column_title,
+        show_row_number         = show_row_number,
+        title                   = title,
+        title_alignment         = title_alignment
+    )
 
     # Select the appropriate backend.
     if _backend === Val(:text)
