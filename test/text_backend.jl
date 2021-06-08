e1a00fb4a7c511bef73296b56669ab22c97134e2
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-06-08 12:55:03 -0300

    :rotating_light: Add tests related to the custom text cells

--- test/text_backend.jl
+++ test/text_backend.jl
@@ -29,6 +29,10 @@ end
     include("text_backend/cropping.jl")
 end
 
+@testset "Custom text cell" begin
+    include("text_backend/custom_cells.jl")
+end
+
 @testset "Errors" begin
     include("text_backend/errors.jl")
 end
