9597d94b0faf14b71549a14c92661a25fb13467c
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2021-08-08 17:29:50 -0300

    :wrench: Remove unused function

--- src/misc.jl
+++ src/misc.jl
@@ -240,15 +240,3 @@ function _process_vlines(vlines::AbstractVector, num_printed_cols::Int)
 
     return Vector{Int}(vlines)
 end
-
-"""
-    _str_escaped(str::AbstractString)
-
-Return the escaped string representation of `str`.
-
-"""
-function _str_escaped(str::AbstractString)
-    # NOTE: Here we cannot use `escape_string(str)` because it also adds the
-    # character `"` to the list of characters to be escaped.
-    return sprint(escape_string, str, "", sizehint = lastindex(str))
-end
