cd3973aa54e77bb5cb1d032d805c8bf78625a643
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2021-09-25 11:31:53 -0300

    :art: Apply BlueStyle to s/b/t/display.jl

--- src/backends/text/display.jl
+++ src/backends/text/display.jl
@@ -13,13 +13,17 @@
 Draw the continuation row when the table has filled the vertical space
 available. This function prints in each column the character `⋮` with the
 alignment in `alignment`.
-
 """
-function _draw_continuation_row(display::Display, io::IO, tf::TextFormat,
-                                text_crayon::Crayon, border_crayon::Crayon,
-                                cols_width::Vector{Int}, vlines::Vector{Int},
-                                alignment::Symbol)
-
+function _draw_continuation_row(
+    display::Display,
+    io::IO,
+    tf::TextFormat,
+    text_crayon::Crayon,
+    border_crayon::Crayon,
+    cols_width::Vector{Int},
+    vlines::Vector{Int},
+    alignment::Symbol
+)
     # In case of a continuation row, we want the last character to indicate that
     # the table continues both vertically and horizontally in case the text is
     # cropped.
@@ -30,7 +34,7 @@ function _draw_continuation_row(display::Display, io::IO, tf::TextFormat,
 
     0 ∈ vlines && _p!(display, border_crayon, tf.column)
 
-    @inbounds for j = 1:num_cols
+    @inbounds for j in 1:num_cols
         data_ij_str  = _str_aligned("⋮", alignment, cols_width[j])
         data_ij_str  = " " * data_ij_str * " "
 
@@ -53,12 +57,18 @@ end
 
 Draw a vertical line in internal line buffer of `display` and then flush to the
 io `io`.
-
 """
-function _draw_line!(display::Display, io::IO, left::Char, intersection::Char,
-                     right::Char, row::Char, border_crayon::Crayon,
-                     cols_width::Vector{Int}, vlines::Vector{Int})
-
+function _draw_line!(
+    display::Display,
+    io::IO,
+    left::Char,
+    intersection::Char,
+    right::Char,
+    row::Char,
+    border_crayon::Crayon,
+    cols_width::Vector{Int},
+    vlines::Vector{Int}
+)
     # We does not want to add ellipsis when drawing lines.
     old_cont_char           = display.cont_char
     old_cont_space_char     = display.cont_space_char
@@ -69,9 +79,9 @@ function _draw_line!(display::Display, io::IO, left::Char, intersection::Char,
 
     0 ∈ vlines && _p!(display, border_crayon, left)
 
-    @inbounds for i = 1:num_cols
+    @inbounds for i in 1:num_cols
         # Check the alignment and print.
-        _p!(display, border_crayon, row^(cols_width[i]+2)) && break
+        _p!(display, border_crayon, row^(cols_width[i] + 2)) && break
 
         i != num_cols &&
             _pc!(i ∈ vlines, display, border_crayon, intersection, "")
@@ -90,10 +100,8 @@ end
     _eol(display::Display)
 
 Return `true` if the cursor is at the end of line or `false` otherwise.
-
 """
-_eol(display::Display) =
-    (display.size[2] > 0) && (display.col >= display.size[2])
+_eol(display::Display) = (display.size[2] > 0) && (display.col >= display.size[2])
 
 """
     _fit_str_to_display!(display::Display, str::Char, final_line_print::Bool = false, lstr::Int = -1)
@@ -115,7 +123,6 @@ The line buffer can be flushed to an `io` using the function `_nl!`.
 - The new string, which is `str` cropped to fit the display.
 - The suffix to be appended to the cropped string.
 - The number of columns that will be used to print the string and the suffix.
-
 """
 function _fit_str_to_display(
     display::Display,
@@ -221,7 +228,6 @@ end
     _nl!(display::Display, io::IO)
 
 Flush the internal line buffer of `display` into `io`.
-
 """
 function _nl!(display::Display, io::IO)
     # Update the information about the current columns and row of the display.
@@ -249,14 +255,24 @@ The size of the string can be passed to `lstr` to save computational burden. If
 `lstr = -1`, then the string length will be computed inside the function.
 
 The line buffer can be flushed to an `io` using the function `_nl!`.
-
 """
-_p!(display::Display, crayon::Crayon, str::Char, final_line_print::Bool = false,
-    lstr::Int = -1) = _p!(display, crayon, string(str), final_line_print, lstr)
+function _p!(
+    display::Display,
+    crayon::Crayon,
+    str::Char,
+    final_line_print::Bool = false,
+    lstr::Int = -1
+)
+    return _p!(display, crayon, string(str), final_line_print, lstr)
+end
 
-function _p!(display::Display, crayon::Crayon, str::String,
-             final_line_print::Bool = false, lstr::Int = -1)
-
+function _p!(
+    display::Display,
+    crayon::Crayon,
+    str::String,
+    final_line_print::Bool = false,
+    lstr::Int = -1
+)
     _eol(display) && return true
 
     # Compute the new string given the display size.
@@ -283,12 +299,17 @@ not to include the continuation character.
 The size of the strings can be passed to `lstr_true` and `lstr_false` to save
 computational burden. If they are `-1`, then the string lengths will be computed
 inside the function.
-
 """
-function _pc!(cond::Bool, display::Display, crayon::Crayon,
-              str_true::Union{Char,String}, str_false::Union{Char,String},
-              final_line_print::Bool = false, lstr_true::Int = -1,
-              lstr_false::Int = -1)
+function _pc!(
+    cond::Bool,
+    display::Display,
+    crayon::Crayon,
+    str_true::Union{Char,String},
+    str_false::Union{Char,String},
+    final_line_print::Bool = false,
+    lstr_true::Int = -1,
+    lstr_false::Int = -1
+)
     if cond
         return _p!(display, crayon, str_true, final_line_print, lstr_true)
     else
@@ -303,7 +324,9 @@ function _write_to_display!(
     suffix::String,
     num_printed_cols::Int
 )
-    num_printed_cols < 0 && (num_printed_cols = textwidth(str) + textwidth(suffix))
+    if num_printed_cols < 0
+        num_printed_cols = textwidth(str) + textwidth(suffix)
+    end
 
     # Print the with correct formating.
     #
