cb9f180b30624ff4ed84c729bbc9a3dd4871ed3b
Ronan Arraes Jardim Chagas <ronan.arraes@inpe.br>
2021-10-04 09:17:05 -0300

    :art: Apply BlueStyle to s/deprecations.jl

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -10,45 +10,69 @@
 #                       Deprecations introduced in v0.12
 # ==============================================================================
 
-@deprecate(pretty_table(data::Any, header::AbstractVector; kwargs...),
-           pretty_table(data; header = header, kwargs...))
+@deprecate(
+    pretty_table(data::Any, header::AbstractVector; kwargs...),
+    pretty_table(data; header = header, kwargs...)
+)
 
-@deprecate(pretty_table(io::IO, data::Any, header::AbstractVector; kwargs...),
-           pretty_table(io, data; header = header, kwargs...))
+@deprecate(
+    pretty_table(io::IO, data::Any, header::AbstractVector; kwargs...),
+    pretty_table(io, data; header = header, kwargs...)
+)
 
-@deprecate(pretty_table(::Type{String}, data::Any, header::AbstractVector; kwargs...),
-           pretty_table(String, data; header = header, kwargs...))
+@deprecate(
+    pretty_table(::Type{String}, data::Any, header::AbstractVector; kwargs...),
+    pretty_table(String, data; header = header, kwargs...)
+)
 
-@deprecate(pretty_table(::Type{HTML}, data::Any, header::AbstractVector; kwargs...),
-           pretty_table(HTML, data; header = header, kwargs...))
+@deprecate(
+    pretty_table(::Type{HTML}, data::Any, header::AbstractVector; kwargs...),
+    pretty_table(HTML, data; header = header, kwargs...)
+)
 
-@deprecate(pretty_table(data::Any, header::AbstractMatrix; kwargs...),
-           pretty_table(data;
-                        header = tuple([header[i, :] for i = 1:size(header, 1)]...),
-                        kwargs...))
+@deprecate(
+    pretty_table(data::Any, header::AbstractMatrix; kwargs...),
+    pretty_table(
+        data;
+        header = tuple([header[i, :] for i = 1:size(header, 1)]...),
+        kwargs...
+    )
+)
 
-@deprecate(pretty_table(io::IO, data::Any, header::AbstractMatrix; kwargs...),
-           pretty_table(io, data;
-                        header = tuple([header[i, :] for i = 1:size(header, 1)]...),
-                        kwargs...))
+@deprecate(
+    pretty_table(io::IO, data::Any, header::AbstractMatrix; kwargs...),
+    pretty_table(
+        io,
+        data;
+        header = tuple([header[i, :] for i = 1:size(header, 1)]...),
+        kwargs...
+    )
+)
 
-@deprecate(pretty_table(::Type{String}, data::Any, header::AbstractMatrix; kwargs...),
-           pretty_table(String, data;
-                        header = tuple([header[i, :] for i = 1:size(header, 1)]...),
-                        kwargs...))
+@deprecate(
+    pretty_table(::Type{String}, data::Any, header::AbstractMatrix; kwargs...),
+    pretty_table(
+        String,
+        data;
+        header = tuple([header[i, :] for i = 1:size(header, 1)]...),
+        kwargs...
+    )
+)
 
-@deprecate(pretty_table(::Type{HTML}, data::Any, header::AbstractMatrix; kwargs...),
-           pretty_table(HTML, data;
-                        header = tuple([header[i, :] for i = 1:size(header, 1)]...),
-                        kwargs...))
+@deprecate(
+    pretty_table(::Type{HTML}, data::Any, header::AbstractMatrix; kwargs...),
+    pretty_table(
+        HTML,
+        data;
+        header = tuple([header[i, :] for i = 1:size(header, 1)]...),
+        kwargs...
+    )
+)
 
 # Those definitions are necessary to avoid ambiguities. They **must** be removed
 # together with the previous deprecations.
-pretty_table(io::IO, data::AbstractVector; kwargs...) =
-    _pretty_table(io, data; kwargs...)
-
-pretty_table(io::IO, data::AbstractMatrix; kwargs...) =
-    _pretty_table(io, data; kwargs...)
+pretty_table(io::IO, data::AbstractVector; kwargs...) = _pretty_table(io, data; kwargs...)
+pretty_table(io::IO, data::AbstractMatrix; kwargs...) = _pretty_table(io, data; kwargs...)
 
 function pretty_table(::Type{String}, data::AbstractVector; kwargs...)
     io = IOBuffer()
