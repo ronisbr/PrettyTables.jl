b2b8db6177eb38d54e44986a0fd697303352cfb5
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-03-17 22:59:56 -0300

    :bug: Fix escape in LaTeX
    We should never use `\\\\` in LaTeX because it is a new line inside a
    table. We changed those situations to `\textbackslash{}`.

--- src/backends/latex/string.jl
+++ src/backends/latex/string.jl
@@ -16,22 +16,21 @@ function _str_latex_escaped(io::IO, s::AbstractString, esc::String = "")
         if c in esc
             print(io, '\\', c)
         elseif isascii(c)
-            c == '\0'          ? print(io, escape_nul(peek(a))) :
-            c == '\e'          ? print(io, "\\e") :
-          # c == '\\'          ? print(io, "\\\\") :
-            '\a' <= c <= '\r'  ? print(io, '\\', "abtnvfr"[Int(c)-6]) :
+            c == '\0'          ? print(io, "\\textbackslash{}0") :
+            c == '\e'          ? print(io, "\\textbackslash{}e") :
+            '\a' <= c <= '\r'  ? print(io, "\\textbackslash{}", "abtnvfr"[Int(c)-6]) :
             c == '%'           ? print(io, "\\%") :
             isprint(c)         ? print(io, c) :
-                                 print(io, "\\x", string(UInt32(c), base = 16, pad = 2))
+                                 print(io, "\\textbackslash{}x", string(UInt32(c), base = 16, pad = 2))
         elseif !isoverlong(c) && !ismalformed(c)
             isprint(c)         ? print(io, c) :
-            c <= '\x7f'        ? print(io, "\\x", string(UInt32(c), base = 16, pad = 2)) :
-            c <= '\uffff'      ? print(io, "\\u", string(UInt32(c), base = 16, pad = Base.need_full_hex(peek(a)) ? 4 : 2)) :
-                                 print(io, "\\U", string(UInt32(c), base = 16, pad = Base.need_full_hex(peek(a)) ? 8 : 4))
+            c <= '\x7f'        ? print(io, "\\textbackslash{}x", string(UInt32(c), base = 16, pad = 2)) :
+            c <= '\uffff'      ? print(io, "\\textbackslash{}u", string(UInt32(c), base = 16, pad = Base.need_full_hex(peek(a)) ? 4 : 2)) :
+                                 print(io, "\\textbackslash{}U", string(UInt32(c), base = 16, pad = Base.need_full_hex(peek(a)) ? 8 : 4))
         else # malformed or overlong
             u = bswap(reinterpret(UInt32, c))
             while true
-                print(io, "\\x", string(u % UInt8, base = 16, pad = 2))
+                print(io, "\\textbackslash{}x", string(u % UInt8, base = 16, pad = 2))
                 (u >>= 8) == 0 && break
             end
         end
