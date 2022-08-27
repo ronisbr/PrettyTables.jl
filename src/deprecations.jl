fce8877ad662bd0118b7516eb63c4f7b96086684
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2022-08-27 18:17:17 -0300

    :warning: :poop: Remove all deprecations

--- src/deprecations.jl
+++ src/deprecations.jl
@@ -6,26 +6,3 @@
 #   Deprecations.
 #
 # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
-
-#                       Deprecations introduced in v2.0
-# ==============================================================================
-
-function _rm_rownum_header_crayon(;
-    rownum_header_crayon::Crayon,
-    kwargs...
-)
-    return kwargs
-end
-
-function _rm_noheader(; noheader::Bool, kwargs...)
-    return kwargs
-end
-
-function _rm_nosubheader(; nosubheader::Bool, kwargs...)
-    return kwargs
-end
-
-@deprecate HTMLDecoration(args...; kwargs...) HtmlDecoration(args...; kwargs...)
-@deprecate HTMLTableFormat(args...; kwargs...) HtmlTableFormat(args...; kwargs...)
-@deprecate HTMLHighlighter(args...; kwargs...) HtmlHighlighter(args...; kwargs...)
-@deprecate URLTextCell(args...; kwargs...) UrlTextCell(args...; kwargs...)
