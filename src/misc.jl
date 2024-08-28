c0ed6e3a92290f25e283b42480346c9b4a633a5b
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2024-08-27 21:55:13 -0300

    :sparkles: Add function to align a column to a regex

--- src/misc.jl
+++ src/misc.jl
@@ -74,6 +74,129 @@ function _aprintln(
 end
 
 """
+    _align_column_with_regex!(column::AbstractVector{Vector{String}}, alignment_anchor_regex::Vector{Regex}, alignment_anchor_fallback::Symbol) -> Int
+
+Align the lines in the `column` at the first match obtained by the regex vector
+`alignment_anchor_regex`, falling back to `alignment_anchor_fallback` if a match is not
+found for a specific line.
+
+This function returns the largest cell width.
+
+!!! note
+
+    Each element in `column` must be a vector of `String`s, where each string is a line.
+"""
+function _align_column_with_regex!(
+    column::AbstractVector{Vector{String}},
+    alignment_anchor_regex::Vector{Regex},
+    alignment_anchor_fallback::Symbol,
+)
+    # Variable to store in which column we must align the match.
+    alignment_column = 0
+
+    # We need to pass through the entire column searching for matches to compute in which
+    # column we need to align them.
+    for row in column
+        for line in row
+            m = nothing
+
+            for r in alignment_anchor_regex
+                m_r = findfirst(r, line)
+                isnothing(m_r) && continue
+                m = m_r
+                break
+            end
+
+            if !isnothing(m)
+                @show alignment_column_i = textwidth(@views line[1:first(m)])
+            else
+                # If a match is not found, the alignment column depends on the user
+                # selection.
+
+                if alignment_anchor_fallback == :c
+                    line_len = textwidth(line)
+                    alignment_column_i = cld(line_len, 2)
+
+                elseif alignment_anchor_fallback == :r
+                    line_len = textwidth(line)
+                    alignment_column_i = line_len + 1
+
+                else
+                    alignment_column_i = 0
+                end
+            end
+
+            alignment_column = max(alignment_column, alignment_column_i)
+        end
+    end
+
+    # Variable to store the largest cell width after the alignment.
+    largest_cell_width = 0
+
+    # Now, we need to pass again applying the alignments.
+    for i in eachindex(column)
+        row = column[i]
+
+        for l in eachindex(row)
+            line = row[l]
+
+            m = nothing
+
+            for r in alignment_anchor_regex
+                m_r = findfirst(r, line)
+                isnothing(m_r) && continue
+                m = m_r
+                break
+            end
+
+            if !isnothing(m)
+                match_column_k = textwidth(@views(line[1:first(m)]))
+                pad = alignment_column - match_column_k
+            else
+                # If a match is not found, the alignment column depends on the user
+                # selection.
+
+                if alignment_anchor_fallback == :c
+                    line_len = textwidth(line)
+                    pad = alignment_column - cld(line_len, 2)
+
+                elseif alignment_anchor_fallback == :r
+                    line_len = textwidth(line)
+                    pad = alignment_column - line_len - 1
+
+                else
+                    pad = alignment_column
+                end
+            end
+
+            # `pad` must be positive.
+            pad = max(pad, 0)
+
+            # Apply the alignment to the line.
+            row[l] = " "^pad * line
+            line_len = textwidth(row[l])
+
+            largest_cell_width = max(largest_cell_width, line_len)
+        end
+    end
+
+    # The third pass aligns the elements correctly. This is performed by adding spaces to
+    # the right so that all the cells have the same width.
+    for i in eachindex(column)
+        row = column[i]
+
+        for l in eachindex(row)
+            line = row[l]
+            pad = largest_cell_width - textwidth(line)
+            pad = min(pad, 0)
+            row[l] = line * " "^pad
+        end
+    end
+
+    return largest_cell_width
+end
+
+"""
     _compact_type_str(T) -> String
 
 Return a string with a compact representation of type `T`.
