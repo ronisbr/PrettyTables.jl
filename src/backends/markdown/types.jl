b2bbac5c254133082dcf3e9980a602c94e4ed4b1
Ronan Arraes Jardim Chagas <ronan.arraes@MacBook-Pro-de-Ronan.local>
2024-09-20 11:07:32 -0300

    :sparkles: [Markdown] Add MarkdownTableStyle

--- src/backends/markdown/types.jl
+++ src/backends/markdown/types.jl
@@ -4,7 +4,7 @@
 #
 ############################################################################################
 
-export MarkdownDecoration, MarkdownHighlighter
+export MarkdownDecoration, MarkdownHighlighter, MarkdownTableFormat, MarkdownTableStyle
 
 """
     struct MarkdownDecoration
@@ -91,24 +91,63 @@ const _MARKDOWN__BOLD          = MarkdownDecoration(bold   = true)
 const _MARKDOWN__ITALIC        = MarkdownDecoration(italic = true)
 const _MARKDOWN__CODE          = MarkdownDecoration(code   = true)
 
+"""
+    struct MarkdownTableFormat
+
+Define the format of the tables printed with the markdown back end.
+
+# Fields
+
+- `title_heading_level::Int`: Title heading level.
+- `subtitle_heading_level::Int`: Subtitle heading level.
+- `horizontal_line_char::Char`: Character used to draw the horizontal line.
+- `line_before_summary_rows::Bool`: Whether to draw a line before the summary rows.
+"""
 @kwdef struct MarkdownTableFormat
     title_heading_level::Int       = 1
     subtitle_heading_level::Int    = 2
     horizontal_line_char::Char     = '─'
     line_before_summary_rows::Bool = true
-
-    # == Row Decorations ===================================================================
-
-    row_number_label_decoration::MarkdownDecoration     = _MARKDOWN__BOLD
-    row_number_decoration::MarkdownDecoration           = _MARKDOWN__BOLD
-    stubhead_label_decoration::MarkdownDecoration       = _MARKDOWN__BOLD
-    row_label_decoration::MarkdownDecoration            = _MARKDOWN__BOLD
-    row_group_label_decoration::MarkdownDecoration      = _MARKDOWN__BOLD
-    first_column_label_decoration::MarkdownDecoration   = _MARKDOWN__BOLD
-    column_label_decoration::MarkdownDecoration         = _MARKDOWN__CODE
-    summary_row_label_decoration::MarkdownDecoration    = _MARKDOWN__BOLD
-    summary_row_cell_decoration::MarkdownDecoration     = _MARKDOWN__NO_DECORATION
-    footnote_decoration::MarkdownDecoration             = _MARKDOWN__NO_DECORATION
-    source_note_decoration::MarkdownDecoration          = _MARKDOWN__NO_DECORATION
-    omitted_cell_summary_decoration::MarkdownDecoration = _MARKDOWN__ITALIC
 end
+
+"""
+    struct MarkdownTableStyle
+
+Define the style of the tables printed with the markdown back end.
+
+# Fields
+
+- `row_number_label::MarkdownDecoration`: Style for the row number label.
+- `row_number::MarkdownDecoration`: Style for the row number.
+- `stubhead_label::MarkdownDecoration`: Style for the stubhead label.
+- `row_label::MarkdownDecoration`: Style for the row label.
+- `row_group_label::MarkdownDecoration`: Style for the row group label.
+- `first_column_label::MarkdownDecoration`: Style for the first line of the  column
+    labels.
+- `column_label::MarkdownDecoration`: Style for the column label.
+- `summary_row_label::MarkdownDecoration`: Style for the summary row label.
+- `summary_row_cell::MarkdownDecoration`: Style for the summary row cell.
+- `footnote::MarkdownDecoration`: Style for the footnote.
+- `source_note::MarkdownDecoration`: Style for the source note.
+- `omitted_cell_summary::MarkdownDecoration`: Style for the omitted cell summary.
+"""
+@kwdef struct MarkdownTableStyle
+    row_number_label::MarkdownDecoration     = _MARKDOWN__BOLD
+    row_number::MarkdownDecoration           = _MARKDOWN__BOLD
+    stubhead_label::MarkdownDecoration       = _MARKDOWN__BOLD
+    row_label::MarkdownDecoration            = _MARKDOWN__BOLD
+    row_group_label::MarkdownDecoration      = _MARKDOWN__BOLD
+    first_column_label::MarkdownDecoration   = _MARKDOWN__BOLD
+    column_label::MarkdownDecoration         = _MARKDOWN__CODE
+    summary_row_label::MarkdownDecoration    = _MARKDOWN__BOLD
+    summary_row_cell::MarkdownDecoration     = _MARKDOWN__NO_DECORATION
+    footnote::MarkdownDecoration             = _MARKDOWN__NO_DECORATION
+    source_note::MarkdownDecoration          = _MARKDOWN__NO_DECORATION
+    omitted_cell_summary::MarkdownDecoration = _MARKDOWN__ITALIC
+end
+
+# Default markdown table format.
+const _MARKDOWN__DEFAULT_TABLE_FORMAT = MarkdownTableFormat()
+
+# Default markdown table style.
+const _MARKDOWN__DEFAULT_TABLE_STYLE = MarkdownTableStyle()
