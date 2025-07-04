24b1a1b7b5fddc0ffc6d68a245376f9ba1443b9d
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2025-07-04 18:38:47 -0300

    :rotating_light: [Markdown] Increase test coverage

--- test/backends/markdown/full.jl
+++ test/backends/markdown/full.jl
@@ -47,6 +47,46 @@ Source Notes
         )
 
         @test result == expected
+
+        expected = """
+# Table Title
+
+## Table Subtitle
+
+|      **Rows** | **Col. 1**<br>`1` | **Merged Column[^1]**<br>`2` | ─────── |
+|--------------:|------------------:|-----------------------------:|--------:|
+|     **Row 1** |            (1, 1) |                       (1, 2) |  (1, 3) |
+| **Row Group** | ───────────────── | ──────────────────────────── | ─────── |
+|     **Row 2** |            (2, 1) |                   (2, 2)[^2] |  (2, 3) |
+|     **Row 3** |            (3, 1) |                       (3, 2) |  (3, 3) |
+| ───────────── | ───────────────── | ──────────────────────────── | ─────── |
+| **Summary 1** |                10 |                           20 |      30 |
+| **Summary 2** |                20 |                           40 |      60 |
+
+[^1]: Footnote in column label
+[^2]: Footnote in data
+
+Source Notes
+"""
+
+        result = pretty_table(
+            String,
+            matrix;
+            backend = :markdown,
+            column_labels = [["Col. $i" for i in 1:3], ["$i" for i in 1:3]],
+            footnotes = [(:column_label, 1, 2) => "Footnote in column label", (:data, 2, 2) => "Footnote in data"],
+            merge_column_label_cells = [MergeCells(1, 2, 2, "Merged Column", :c)],
+            row_group_labels = [2 => "Row Group"],
+            row_labels = ["Row $i" for i in 1:5],
+            show_row_number_column = false,
+            source_notes = "Source Notes",
+            stubhead_label = "Rows",
+            subtitle = "Table Subtitle",
+            summary_rows = [(data, i) -> 10i, (data, i) -> 20i],
+            title = "Table Title",
+        )
+
+        @test result == expected
     end
 
     @testset "With Bottom Cropping" begin
