8d6c883f2499bfeb2dc96b6f2450c79929509850
Gustavo Henrique Tavares Cardoso <gustavo.cardoso@brb.com.br>
2025-12-23 12:24:17 -0300

    test: backend typst - decorations - testset

---
+++ test/backends/typst/decorations.jl
@@ -0,0 +1,64 @@
+## Description #############################################################################
+#
+# HTML Back End: Tests related with decorations.
+#
+############################################################################################
+
+@testset "Decorations" verbose = true begin
+    @testset "Decoration of Column Labels" begin
+        matrix = ones(3, 3)
+        backend = :typst
+
+        expected = """
+#{
+  table(
+    columns: (auto, auto, auto), 
+    table.header(
+        table.cell(align: right,fill: yellow,)[#text()[Col. 1]],table.cell(align: right,fill: yellow,)[#text()[Col. 2]],table.cell(align: right,fill: yellow,)[#text()[Col. 3]],
+    ), 
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+  )
+}
+"""
+
+        result = pretty_table(
+            String,
+            matrix;
+            backend,
+            color   = true,
+            style   = TypstTableStyle(; first_line_column_label = ["fill" => "yellow"])
+        )
+
+        @test result == expected
+
+        expected = """
+#{
+  table(
+    columns: (auto, auto, auto), 
+    table.header(
+        table.cell(align: right,fill: yellow,)[#text()[Col. 1]],table.cell(align: right,fill: blue,)[#text()[Col. 2]],table.cell(align: right,fill: red,)[#text()[Col. 3]],
+    ), 
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+  )
+}
+"""
+
+        result = pretty_table(
+            String,
+            matrix;
+            backend,
+            style   = TypstTableStyle(; first_line_column_label = [
+                ["fill" => "yellow"],
+                ["fill" => "blue"],
+                ["fill" => "red"]
+            ])
+        )
+
+        @test result == expected
+    end
+end
+
