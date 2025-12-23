88b91ff498a100660353417720657d6c22b1f641
Gustavo Henrique Tavares Cardoso <gustavo.cardoso@brb.com.br>
2025-12-23 12:17:56 -0300

    test: backend typst - offset_arrays - testset

---
+++ test/backends/typst/offset_arrays.jl
@@ -0,0 +1,40 @@
+## Description #############################################################################
+#
+# HTML Back End: Test related with offset arrays.
+#
+############################################################################################
+
+@testset "Offset Arrays" begin
+    backend = :typst
+    matrix = Matrix{Any}(undef, 3, 3)
+    matrix[1, 1] = (1, 1)
+    matrix[1, 2] = (1, 2)
+    matrix[2, 1] = nothing
+    matrix[2, 2] = missing
+    matrix[3, 3] = (3, 3)
+
+    omatrix = OffsetArray(matrix, -2:0, -3:-1)
+
+    expected = raw"""
+#{
+  table(
+    columns: (auto, auto, auto, auto), 
+    table.header(
+        table.cell(align: right,)[#text(weight: "bold",)[Row]],table.cell(align: right,)[#text(weight: "bold",)[Col. -3]],table.cell(align: right,)[#text(weight: "bold",)[Col. -2]],table.cell(align: right,)[#text(weight: "bold",)[Col. -1]],
+    ), 
+    table.cell(align: right,)[#text(weight: "bold",)[-2]],table.cell(align: right,)[#text()[(1, 1)]],table.cell(align: right,)[#text()[(1, 2)]],table.cell(align: right,)[#text()[\#undef]],
+    table.cell(align: right,)[#text(weight: "bold",)[-1]],table.cell(align: right,)[#text()[nothing]],table.cell(align: right,)[#text()[missing]],table.cell(align: right,)[#text()[\#undef]],
+    table.cell(align: right,)[#text(weight: "bold",)[0]],table.cell(align: right,)[#text()[\#undef]],table.cell(align: right,)[#text()[\#undef]],table.cell(align: right,)[#text()[(3, 3)]],
+  )
+}
+"""
+
+    result = pretty_table(
+        String,
+        omatrix;
+        backend,
+        show_row_number_column = true,
+    )
+    write("d:/estudos/20250909 - Análise Card/print_tabela.$(backend == :typst ? "typ" : "html")", result)
+    @test result == expected
+end
