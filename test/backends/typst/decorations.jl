c750550bf0d7127e77ac99042fb00a1e52edf91a
Gustavo Henrique Tavares Cardoso <gustavo.cardoso@brb.com.br>
2025-12-23 12:30:11 -0300

    test: backend typst - decorations - testset

--- test/backends/typst/decorations.jl
+++ test/backends/typst/decorations.jl
@@ -59,6 +59,34 @@
         )
 
         @test result == expected
+
+
+        expected = """
+#{
+  table(
+    columns: (auto, auto, auto), 
+    table.header(
+        table.cell(align: right,fill: yellow,)[#text(fill: blue,weight: "extrabold",)[Col. 1]],table.cell(align: right,fill: blue,)[#text(fill: white,weight: "extrabold",)[Col. 2]],table.cell(align: right,fill: red,)[#text(fill: rgb(30,30,30),)[Col. 3]],
+    ), 
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+    table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1.0]],
+  )
+}
+"""
+
+        result = pretty_table(
+            String,
+            matrix;
+            backend,
+            style   = TypstTableStyle(; first_line_column_label = [
+                ["fill" => "yellow","text-fill"=>"blue", "text-weight"=>"extrabold"],
+                ["fill" => "blue","text-fill"=>"white", "text-weight"=>"extrabold"],
+                ["fill" => "red", "text-fill"=>"rgb(30,30,30)"]
+            ])
+        )
+
+        @test result == expected
     end
 end
 
