e0826b5c0a9237a4d533e730fc51b896bed73519
Gustavo Henrique Tavares Cardoso <gustavo.cardoso@brb.com.br>
2025-12-29 10:47:53 -0300

    tests: add Typstry extension tests

--- test/ext/pretty_tables_typstry_ext.jl
+++ test/ext/pretty_tables_typstry_ext.jl
@@ -12,9 +12,46 @@ using Typstry
         2 2.0 0x02 'b' "def" nothing
         3 3.0 0x03 'c' "ghi" :symbol
     ]
+  @testset "Test Typst Type ouptut" verbose = true begin
+    text_expected = """#{
+  table(
+    columns: (auto, auto, auto, auto, auto, auto), 
+    table.header(
+        table.cell(align: right,)[#text(weight: "bold",)[Col. 1]],table.cell(align: right,)[#text(weight: "bold",)[Col. 2]],table.cell(align: right,)[#text(weight: "bold",)[Col. 3]],table.cell(align: right,)[#text(weight: "bold",)[Col. 4]],table.cell(align: right,)[#text(weight: "bold",)[Col. 5]],table.cell(align: right,)[#text(weight: "bold",)[Col. 6]],
+    ), 
+    table.cell(align: right,)[#text()[1]],table.cell(align: right,)[#text()[1.0]],table.cell(align: right,)[#text()[1]],table.cell(align: right,)[#text()[a]],table.cell(align: right,)[#text()[abc]],table.cell(align: right,)[#text()[missing]],
+    table.cell(align: right,)[#text()[2]],table.cell(align: right,)[#text()[2.0]],table.cell(align: right,)[#text()[2]],table.cell(align: right,)[#text()[b]],table.cell(align: right,)[#text()[def]],table.cell(align: right,)[#text()[nothing]],
+    table.cell(align: right,)[#text()[3]],table.cell(align: right,)[#text()[3.0]],table.cell(align: right,)[#text()[3]],table.cell(align: right,)[#text()[c]],table.cell(align: right,)[#text()[ghi]],table.cell(align: right,)[#text()[symbol]],
+  )
+}
+"""
+    # Test String Output
+    text_result = pretty_table(
+      String,
+      matrix;
+      backend
+    )
+    @test text_result == text_expected
 
-    @testset "Alignment as a Symbol" verbose = true  begin
-      expected = """"""
-      true
-    end
+    # Test Typst Output
+
+    expected = Typst(TypstText(text_expected))
+
+    result = pretty_table(
+      Typst,
+      matrix;
+      backend
+    )
+
+    @test result == expected
+
+    # Test backend inferred by Typst Output
+    result_inferred = pretty_table(
+      Typst,
+      matrix;
+    )
+
+    @test expected == result_inferred
+
+  end
 end
