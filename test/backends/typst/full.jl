f5664f4869c0bdb8a628d33a9e39a77a19c61ee0
Gustavo Henrique Tavares Cardoso <gustavo.cardoso@brb.com.br>
2026-01-22 11:45:06 -0300

    [Typst][tests] Add Test to wrap_column

--- test/backends/typst/full.jl
+++ test/backends/typst/full.jl
@@ -452,5 +452,82 @@
         @test result == expected
 
     end
+    @testset "Wrap Column Tests" begin
+        expected = """
+#{
+  set par(justify: true, spacing: 1em)
+  align(top+right,)[2 columns and 2 rows omitted]
+  table(
+    columns: (auto, auto, auto, auto, auto),
+    table.header(
+      table.cell(align: center, colspan: 5,)[#text(size: 1.1em, weight: "bold",)[Table Title]],
+      table.cell(align: center, colspan: 5,)[#text(size: 1.1em, style: "italic",)[Table Subtitle]],
+      table.cell(align: right,)[#text(weight: "bold",)[Row]],
+      table.cell(align: right,)[#text(weight: "bold",)[Rows]],
+      table.cell(align: right,)[#text(weight: "bold",)[Col. 1]],
+      table.cell(align: center, colspan: 1, stroke: (paint: rgb(200,200,200), thickness: 0.01pt),)[
+        #text(weight: "bold",)[Merged Column]#super[1]
+      ],
+      table.cell()[#text()[ ⋯ ]],
+    ),
+    table.cell(align: right,)[#text(weight: "bold",)[1]],
+    table.cell(align: right,)[#text(weight: "bold",)[Row 1]],
+    table.cell(align: right,)[#text()[(1, 1)]],
+    table.cell(align: right,)[#text()[(1, 2)]],
+    table.cell()[#text()[ ⋯ ]],
+    table.cell()[#text()[  ⋮ ]],
+    table.cell()[#text()[  ⋮ ]],
+    table.cell()[#text()[  ⋮ ]],
+    table.cell()[#text()[  ⋮ ]],
+    table.cell()[#text()[ ⋱ ]],
+    table.cell(align: right,)[#text(weight: "bold",)[4]],
+    table.cell(align: right,)[#text(weight: "bold",)[Row 4]],
+    table.cell(align: right,)[#text()[(4, 1)]],
+    table.cell(align: right,)[#text()[(4, 2)]],
+    table.cell()[#text()[ ⋯ ]],
+    table.cell(align: right,)[#text(weight: "bold",)[]],
+    table.cell(align: right,)[#text(weight: "bold",)[Summary 1]],
+    table.cell(align: right,)[#text()[10]],
+    table.cell(align: right,)[#text()[20]],
+    table.cell()[#text()[ ⋯ ]],
+    table.cell(align: right,)[#text(weight: "bold",)[]],
+    table.cell(align: right,)[#text(weight: "bold",)[Summary 2]],
+    table.cell(align: right,)[#text()[20]],
+    table.cell(align: right,)[#text()[40]],
+    table.cell()[#text()[ ⋯ ]],
+    table.cell(align: left, colspan: 5,)[#super[1]#text(size: 0.9em,)[Footnote in column label]],
+    table.cell(align: left, colspan: 5,)[#super[2]#text(size: 0.9em,)[Footnote in data]],
+    table.cell(align: left, colspan: 5,)[#text(fill: gray, size: 0.9em, style: "italic",)[Source Notes]],
+  )
+}
+"""
+
+        result = pretty_table(
+            String,
+            matrix;
+            backend,
+            footnotes = [
+                (:column_label, 1, 2) => "Footnote in column label",
+                (:data, 2, 2) => "Footnote in data",
+            ],
+            maximum_number_of_columns = 2,
+            maximum_number_of_rows = 2,
+            merge_column_label_cells = [MergeCells(1, 2, 2, "Merged Column", :c)],
+            row_group_labels = [2 => "Row Group"],
+            row_labels = ["Row $i" for i in 1:5],
+            show_row_number_column = true,
+            source_notes = "Source Notes",
+            stubhead_label = "Rows",
+            subtitle = "Table Subtitle",
+            summary_rows = [(data, i) -> 10i, (data, i) -> 20i],
+            title = "Table Title",
+            vertical_crop_mode = :middle,
+            wrap_column=1, #any attribute will need a new line
+            annotate=false
+        )
+
+        @test result == expected
+
+    end
 
 end
