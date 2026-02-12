804d971ce088b0d7f0bf08f4037fc608b985edd5
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-02-12 15:17:41 -0300

    :books: [Typst] Fix typo

--- ext/PrettyTablesTypstryExt.jl
+++ ext/PrettyTablesTypstryExt.jl
@@ -2,7 +2,7 @@ module PrettyTablesTypstryExt
 using PrettyTables, Typstry
 
 function PrettyTables.pretty_table(::Type{Typst}, @nospecialize(data::Any); kwargs...)
-    # If the keywords does not set the back end or the table format, use the HTML back end
+    # If the keywords does not set the back end or the table format, use the Typst back end
     # by default.
     str = if !haskey(kwargs, :backend)
         pretty_table(String, data; backend = :typst, kwargs...)
