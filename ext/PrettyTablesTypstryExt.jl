6e4db0b1f173fc6da8bd669d1bebc71777362061
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-02-12 15:29:57 -0300

    :wrench: [Typst] Support Markdown if Typstry is loaded

--- ext/PrettyTablesTypstryExt.jl
+++ ext/PrettyTablesTypstryExt.jl
@@ -1,5 +1,9 @@
 module PrettyTablesTypstryExt
-using PrettyTables, Typstry
+
+using PrettyTables
+
+using Markdown
+using Typstry
 
 function PrettyTables.pretty_table(::Type{Typst}, @nospecialize(data::Any); kwargs...)
     # If the keywords does not set the back end or the table format, use the Typst back end
@@ -11,6 +15,15 @@ function PrettyTables.pretty_table(::Type{Typst}, @nospecialize(data::Any); kwar
     end
 
     return Typst(TypstText(str))
+end
+
+# For Markdown cells, we must render always using `show` to obtain the correct decoration.
+function PrettyTables._typst__render_cell(
+    cell::Markdown.MD,
+    context::IOContext,
+    renderer::Union{Val{:print}, Val{:show}},
+)
+    return sprint(show, MIME("text/typst"), Typst(cell))
 end
 
 end
