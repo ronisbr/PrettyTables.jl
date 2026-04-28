618656039bc1acbe88fc62f5df9ea196ec977b75
Ronan Arraes Jardim Chagas <ronisbr@gmail.com>
2026-04-28 20:54:39 -0300

    :wrench: [Excel] Update the default format and style

--- src/backends/excel/types.jl
+++ src/backends/excel/types.jl
@@ -269,22 +269,22 @@ Define the table borders that will be used to form the Excel table.
 
     # == Configuration for the Horizontal and Vertical Lines ===============================
 
-    horizontal_line_at_beginning::Bool                         = true
-    horizontal_line_after_column_labels::Bool                  = true
-    horizontal_line_between_column_labels::Bool                = true
-    horizontal_line_at_merged_column_labels::Bool              = true
-    horizontal_lines_at_data_rows::Union{Symbol, Vector{Int}}  = :all
-    horizontal_line_after_data_rows::Bool                      = true
-    horizontal_line_before_row_group_label::Bool               = true
-    horizontal_line_after_row_group_label::Bool                = true
-    horizontal_line_before_summary_rows::Bool                  = true
-    horizontal_line_after_summary_rows::Bool                   = true
+    horizontal_line_at_beginning::Bool = true
+    horizontal_line_after_column_labels::Bool = true
+    horizontal_line_between_column_labels::Bool = false
+    horizontal_line_at_merged_column_labels::Bool = true
+    horizontal_lines_at_data_rows::Union{Symbol, Vector{Int}} = :none
+    horizontal_line_after_data_rows::Bool = true
+    horizontal_line_before_row_group_label::Bool = true
+    horizontal_line_after_row_group_label::Bool = true
+    horizontal_line_before_summary_rows::Bool = true
+    horizontal_line_after_summary_rows::Bool = true
 
-    vertical_line_at_beginning::Bool                           = true
-    vertical_line_after_row_number_column::Bool                = true
-    vertical_line_after_row_label_column::Bool                 = true
+    vertical_line_at_beginning::Bool = true
+    vertical_line_after_row_number_column::Bool = true
+    vertical_line_after_row_label_column::Bool = true
     vertical_lines_at_data_columns::Union{Symbol, Vector{Int}} = :all
-    vertical_line_after_data_columns::Bool                     = true
+    vertical_line_after_data_columns::Bool = true
 end
 
 ############################################################################################
